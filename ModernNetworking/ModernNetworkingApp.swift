e488002b674522e05d85500d5237ed6305dbe493
Mark Kazakov <markazakov@gmail.com>
2025-04-04 20:45:32 +0300

    Initial Commit

---
+++ ModernNetworking/ModernNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernNetworkingApp.swift
+//  ModernNetworking
+//
+//  Created by Mark Kazakov on 04/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ModernNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
